f52daf653043ed4d8e1f81bf1174f37a21a44a73
Vitor Hugo <VitorHugoOli@github.com>
2019-09-30 01:38:47 -0300

    first commit

---
+++ lib/initial_page.dart
@@ -0,0 +1,169 @@
+import 'dart:convert';
+
+import 'package:connected_car/usuario/enter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  bool isLoggedIn = false;
+  var prof = "";
+
+  void onLoginStatusChanged(bool isLoggedIn, {profileData}) {
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
+
+  void initiateFacebookLogin() async {
+    var facebookLogin = FacebookLogin();
+    var facebookLoginResult =
+        await facebookLogin.logInWithReadPermissions(['email']);
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+
+        var graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${facebookLoginResult.accessToken.token}');
+
+        var profile = json.decode(graphResponse.body);
+        prof = profile.toString();
+
+        print(profile.toString());
+
+        onLoginStatusChanged(true, profileData: profile);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+//      bottomNavigationBar: BottomNavigationBar(
+//        items: <BottomNavigationBarItem>[
+//          BottomNavigationBarItem(
+//              icon: Icon(Icons.directions_car), title: Text("List car")),
+//          BottomNavigationBarItem(
+//              icon: Icon(Icons.directions), title: Text("vai")),
+//        ],
+//        backgroundColor: Colors.lightBlue,
+//      ),
+
+      body: Container(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/initial.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 200.0,
+              ),
+              Center(
+                child: Text(
+                  "Bem Vindo\n ao Car Connect",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'GoogleSans',
+                    fontSize: 50,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              Container(
+                child: SizedBox(
+                  height: 38,
+                  width: 212,
+                  child: RaisedButton(
+                    child: (Text(
+                      "Login in | Cadastro",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontFamily: 'GoogleSans'),
+                    )),
+                    color: Colors.blueGrey[500],
+                    onPressed: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => LoginSignUpPage()),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Container(
+                child: SizedBox(
+                  child: isLoggedIn
+                      ? Text("Logged in as: ${prof}")
+                      : RaisedButton(
+                          child: (Text(
+                            "Login with Facebook",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'GoogleSans'),
+                          )),
+                          color: Colors.blue,
+                          onPressed: () => initiateFacebookLogin(),
+                        ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
